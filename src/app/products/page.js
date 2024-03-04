5ce43735d93dc61359946511fe50dc85238de81d
krimamaheta <krimamaheta2002@gmail.com>
2024-03-04 12:28:29 +0530

    static html

---
+++ src/app/products/page.js
@@ -0,0 +1,9 @@
+const Products=()=>{
+    return(
+        <div>
+            <h2>product page</h2>
+        </div>
+    )
+}
+
+export default Products;
---
+++ src/app/sellers/page.js
@@ -0,0 +1,13 @@
+import User from "../../components/user"
+
+const Selles=()=>{
+
+    return(
+        <div>
+            <h2>Selles page</h2>
+            <User />
+        </div>
+    )
+}
+
+export default Selles;
