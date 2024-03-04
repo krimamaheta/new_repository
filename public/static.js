117c66b1e674d6740520e196d579f8a0c9d1ed43
krimamaheta <krimamaheta2002@gmail.com>
2024-03-04 12:27:26 +0530

    static assets

---
+++ public/static.js
@@ -0,0 +1,2 @@
+
+        console.log("from static assests.............")
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* <link rel='stylesheet'  href='/style.css'/>
+        <script src='static.js'/> */}
+        <link rel="stylesheet" href="/style.css"/>
+        <script src="/static.js" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
